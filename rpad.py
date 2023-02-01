bd2c3a977190145eb245480935bf6ddd72ce3079
Hyuk S. Chung <nacl31566@gmail.com>
2023-02-01 22:58:25 +0900

    v0.4

--- rpad.py
+++ rpad.py
@@ -3,8 +3,13 @@ from tkinter import filedialog
 from tkinter import messagebox
 from tkinter.messagebox import askyesno
 import subprocess
-import pyglet
 import rpy2.robjects as robjects
+import tkinter.ttk
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+import pandas as pd
+
+nowfile=''
 
 def seeend() :
     text.see(END)
@@ -16,12 +21,15 @@ def confirm():
         window.destroy()
 
 def resize(event):
-    pixelX=window.winfo_width()-yscrollbar.winfo_width()
-    pixelY=window.winfo_height()
-    text["width"]=int(round(pixelX/h[1]))
-    text["height"]=int(round(pixelY/h[0]))
-    otext["width"]=int(round(pixelX/h[1]))
-    otext["height"]=7
+    pixelX=window.winfo_width()/2-yscrollbar.winfo_width()*2
+    pixelY1=window.winfo_height()
+    pixelY2=window.winfo_height()-itext.winfo_height()
+    text["width"]=int(round(pixelX/h1[1]))
+    text["height"]=int(round(pixelY1/h1[0]))
+    otext["width"]=int(round(pixelX/h2[1]))
+    otext["height"]=int(round(pixelY2/h2[0]))
+    itext["width"]=int(round(pixelX/h2[1]))
+    itext["height"]=7
 
 def o_empty():
     otext.delete('1.0', END)
@@ -72,10 +80,7 @@ def exit():
         window.destroy()
         
 def about():
-    label = messagebox.showinfo("About", "RPad v0.2 (c) 2023 Hyeok Jeong")
-
-def rse():
-    label = messagebox.showinfo("Rscript", rpath)
+    label = messagebox.showinfo("About", "RPad v0.4 (c) 2023 Hyeok Jeong")
 
 def runcom():
     target = text.get("1.0","insert lineend")
@@ -85,24 +90,29 @@ def runcom():
         print(e)
 
 def runr():
-    otext.delete('insert linestart', 'insert lineend')
-    try:
+    itext.delete('1.0', END)
+    try :
         tline = text.get('insert linestart', 'insert lineend')
         rline = robjects.r(tline)
-        otext.insert('1.0', rline)
+        otext.insert(END, rline)
+        iline = robjects.r('ls.str()')
+        itext.insert('1.0', iline)
         seeend()
         qsave()
-    except : otext.insert('1.0', "Rpad typeerror")
+    except Exception as e : otext.insert('1.0', e)
 
 def runa():
     otext.delete('1.0', END)
-    try:
-        tline = text.get('insert linestart', 'insert lineend')
+    itext.delete('1.0', END)
+    try :
+        tline = text.get('1.0', END)
         rline = robjects.r(tline)
-        otext.insert('1.0', rline)
+        otext.insert(END, rline)
+        iline = robjects.r('ls.str()')
+        itext.insert('1.0', iline)
         seeend()
         qsave()
-    except : otext.insert('1.0', "Rpad typeerror")
+    except Exception as e : otext.insert('1.0', e)
 
 def newfile() :
     file = open("./data/templete.Rmd", 'r', encoding="UTF-8")
@@ -118,29 +128,79 @@ def newfile() :
     o_empty()
     seeend()
 
-def frse() :
-    rsefile = filedialog.askopenfilename(parent=window)
-    f = open("./data/r.txt", 'w', encoding="UTF-8")
-    f.write(rsefile)
-    global rpath
-    rpath = rsefile
+#Pretendard Variable Medium
 
-def mfont():
-    try :
-        var1 = int(text.get('insert linestart', 'insert lineend'))
-        text.configure(font=("Pretendard Variable Medium", var1))
-        otext.configure(font=("Pretendard Variable Medium", var1))
+def font16():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("16")
+        font_size_file.close()
+        text.configure(font=("Courier New", 16, "bold"))
+        otext.configure(font=("Courier New", 16, "bold"))
         text.update()
         otext.update()
-        text.delete('insert linestart', 'insert lineend')
-    except : otext.insert('1.0', "fontsize typeerror")
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
 
-pyglet.font.add_file('./font/PretendardVariable.ttf')
+def font20():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("20")
+        font_size_file.close()
+        text.configure(font=("Courier New", 20, "bold"))
+        otext.configure(font=("Courier New", 20, "bold"))
+        text.update()
+        otext.update()
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
 
-nowfile = ''
-rf = open('./data/r.txt', 'r', encoding='UTF-8')
-rpath = rf.readline()
-rf.close
+def font28():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("28")
+        font_size_file.close()
+        text.configure(font=("Courier New", 28, "bold"))
+        otext.configure(font=("Courier New", 28, "bold"))
+        text.update()
+        otext.update()
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
+
+def font36():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("36")
+        font_size_file.close()
+        text.configure(font=("Courier New", 36, "bold"))
+        otext.configure(font=("Courier New", 36, "bold"))
+        text.update()
+        otext.update()
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
+
+def font48():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("48")
+        font_size_file.close()
+        text.configure(font=("Courier New", 48, "bold"))
+        otext.configure(font=("Courier New", 48, "bold"))
+        text.update()
+        otext.update()
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
+
+def font72():
+        font_size_file = open("./data/fontsize.txt", "w", encoding='UTF-8')
+        font_size_file.write("72")
+        font_size_file.close()
+        text.configure(font=("Courier New", 72, "bold"))
+        otext.configure(font=("Courier New", 72, "bold"))
+        text.update()
+        otext.update()
+        global h1
+        h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+        resize()
 
 #창 생성
 window = Tk()
@@ -149,61 +209,75 @@ window.geometry('1024x768')
 window.iconbitmap('./data/rpadico.ico')
 window.resizable(1,1)
 
-text = Text(master=window, undo=True, fg="#CCCCCC", bg="#202020", insertbackground="#CCCCCC", selectbackground="#007F0E")
-text.grid(row=0, column=0, sticky=W+N+S+E)
-text.configure(font=("Pretendard Variable Medium", 20))
+text = Text(master=window, undo=True, fg="#CCCCCC", bg="#202020", insertbackground="#CCCCCC")
+text.grid(row=0, column=0, rowspan=2, sticky=W+N+S)
+text.configure(font=("Courier New", open("./data/fontsize.txt", "r", encoding='UTF-8').readlines(), "bold"))
 
 yscrollbar = Scrollbar(window, orient=VERTICAL, command=text.yview)
-yscrollbar.grid(row=0, column=1, sticky=N+S+E+W)
+yscrollbar.grid(row=0, column=1, rowspan=2, sticky=N+S+W)
 text["yscrollcommand"]=yscrollbar.set
-text.update()
 
 text.bind("<Control-s>", lambda event: qsave())
+text.bind("<Control-Shift-KeyPress-S>", lambda event: save())
 text.bind("<Control-o>", lambda event: fopen())
 text.bind("<Control-r>", lambda event: runr())
 text.bind("<Control-Shift-KeyPress-R>", lambda event: runa())
 text.bind("<Control-e>", lambda event: o_empty())
-text.bind("<Alt-t>", lambda event: mfont())
+#text.bind("<Control-Up>", lambda event: sfont(cfontsize, 5))
+#text.bind("<Control-Down>", lambda event: sfont(cfontsize, -5))
 
 
-otext = Text(master=window, undo=True, fg="#7FFFFF", bg="#202020", insertbackground="#7FFFFF", selectbackground="#007F0E")
-otext.grid(row=1, column=0, sticky=W+N+S+E)
-otext.configure(font=("Pretendard Variable Medium", 20))
+otext = Text(master=window, undo=True, fg="#7FFFFF", bg="#202020", insertbackground="#7FFFFF")
+otext.grid(row=0, column=2, sticky=N+E+S)
+otext.configure(font=("Courier New", open("./data/fontsize.txt", "r", encoding='UTF-8').readlines(), "bold"))
 
 oyscrollbar = Scrollbar(window, orient=VERTICAL, command=otext.yview)
-oyscrollbar.grid(row=1, column=1, sticky=N+S+E+W)
+oyscrollbar.grid(row=0, column=3, sticky=N+S+W)
 otext["yscrollcommand"]=oyscrollbar.set
+
+itext = Text(master=window, undo=True, fg="#FFFF00", bg="#202020", insertbackground="#FFFF00")
+itext.grid(row=1, column=2, sticky=N+E+S)
+itext.configure(font=("Courier New", open("./data/fontsize.txt", "r", encoding='UTF-8').readlines(), "bold"))
+
+text.update()
 otext.update()
+itext.update()
 
-h=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+h1=int(round(text.winfo_height()/text["height"])), int(round(text.winfo_width()/text["width"]))
+h2=int(round(otext.winfo_height()/otext["height"])), int(round(otext.winfo_width()/otext["width"]))
 
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+window.bind("<Configure>", resize)
 
 #메뉴를 붙인다
 menu = Menu(window)
 window.config(menu=menu)
-filemenu = Menu(menu)
-runmenu = Menu(menu)
+filemenu = Menu(menu, tearoff=0)
+runmenu = Menu(menu, tearoff=0)
+fontmenu = Menu(menu, tearoff=0)
+helpmenu = Menu(menu, tearoff=0)
 
 menu.add_cascade(label="File", menu=filemenu)
 filemenu.add_command(label="New File", command=newfile)
 filemenu.add_command(label="Open...   (Ctrl+O)", command=fopen)
-filemenu.add_command(label="Save as...", command=save)
-filemenu.add_command(label="Find Rscript", command=frse)
-filemenu.add_command(label="Kill queues   (Ctrl+E)", command=o_empty)
+filemenu.add_command(label="Save...    (Ctrl+S)", command=qsave)
+filemenu.add_command(label="Save as...    (Ctrl+Shift+S)", command=save)
+filemenu.add_command(label="Empty Output   (Ctrl+E)", command=o_empty)
 filemenu.add_command(label="Quit", command=exit)
 
 menu.add_cascade(label="Run", menu=runmenu)
 runmenu.add_command(label="Run current line only...   (Ctrl+R)", command=runr)
+runmenu.add_command(label="Run all...   (Ctrl+Shift+R)", command=runa)
 
-helpmenu = Menu(menu, tearoff=0) # 자르는 선
+menu.add_cascade(label="Font", menu=fontmenu)
+fontmenu.add_command(label="16pt", command=font16)
+fontmenu.add_command(label="20pt", command=font20)
+fontmenu.add_command(label="28pt", command=font28)
+fontmenu.add_command(label="36pt", command=font36)
+fontmenu.add_command(label="48pt", command=font48)
+fontmenu.add_command(label="72pt", command=font72)
 
 menu.add_cascade(label="About", menu=helpmenu)
 helpmenu.add_command(label="About Rpad", command=about)
-helpmenu.add_command(label="Rscript", command=rse)
-
-window.bind("<Configure>", resize)
 
 window.protocol( "WM_DELETE_WINDOW", confirm )
 window.mainloop()
